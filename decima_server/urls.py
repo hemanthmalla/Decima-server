85bdfc5fd9c0ea683b24ce00c1b65653a00aa756
Bipul Jain <bipuljain44@gmail.com>
2015-03-29 06:42:53 +0530

    Using function based view method

--- decima_server/urls.py
+++ decima_server/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from rest_api import views
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^snippets/$', views.getContactsInNetwork),
-    url(r'^decima/(?P<key>[\w\d\-]+)/', views.decimaMail),
-    url(r'^makedecision/$', views.makeDecision),
-    url(r'^createuser/$', views.createUser),
-    url(r'^getquestionbyid/$', views.getQuestionById),
-    url(r'^getquestionsbyuser/$', views.getQuestionsByUser),
+    url(r'^snippets/$','rest_api.views.getContactsInNetwork'),
+    url(r'^decima/(?P<key>[\w\d\-]+)/', 'rest_api.views.decimaMail'),
+    url(r'^makedecision/$', 'rest_api.views.makeDecision'),
+    url(r'^createuser/$', 'rest_api.views.createUser'),
+    url(r'^getquestionbyid/$', 'rest_api.views.getQuestionById'),
+    url(r'^getquestionsbyuser/$', 'rest_api.views.getQuestionsByUser'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
 ]
