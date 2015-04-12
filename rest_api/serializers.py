209c0ded0c94819976f4d18ec0f4539d7b517720
Hemanth Malla <hemanthmalla@gmail.com>
2015-04-12 18:59:07 +0530

    Fixing the ****ing serializing bug

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -11,7 +11,6 @@ class OptionSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'full_name', 'email', 'phone')
 
 class UserSerializerSecure(serializers.ModelSerializer):
     class Meta:
