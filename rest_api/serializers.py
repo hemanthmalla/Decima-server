142a03de2ce343d2aa247b3b281e8832fa47c70f
Hemanth Malla <hemanthmalla@gmail.com>
2015-04-30 19:43:06 +0530

    Reverting back the old QuestionSerializer

--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -3,6 +3,8 @@ import logging
 from rest_api.models import *
 
 logger = logging.getLogger(__name__)
+
+
 class OptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Option
@@ -11,6 +13,7 @@ class OptionSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+
 
 class UserSerializerSecure(serializers.ModelSerializer):
     class Meta:
@@ -35,6 +38,15 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class QuestionSerializer(serializers.ModelSerializer):
     products = ProductSerializer(many=True, read_only=True)
+    has_voted = serializers.SerializerMethodField()
+    asked_by = UserSerializerSecure()
+    answers_by = UserSerializerSecure(many=True)
+
+    def get_has_voted(self, obj):
+        user = self.context.get("user_id")
+        logger.debug(user)
+        return Vote.objects.get(user_id_id=user, question=obj).voted
 
     class Meta:
         model = Question
+        depth = 1
